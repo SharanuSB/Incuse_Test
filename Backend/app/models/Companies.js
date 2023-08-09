cb4679e11682d732199a00913fa4e80cbeb352f7
Sharanu <sharanusb12345@gmail.com>
2023-08-09 13:36:39 +0530

    Frontend and Backend Updated

---
+++ Backend/app/models/Companies.js
@@ -0,0 +1,12 @@
+
+import mongoose from "mongoose";
+
+const {Schema} = mongoose
+
+const companySchema = new Schema({
+
+})
+
+const Company = mongoose.model("Company", companySchema)
+
+export default Company
